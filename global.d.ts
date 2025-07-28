0a77eebd8c63fac0ba61a3d5c3b91d04f58fd66d
Will Gigan <willgigan244@gmail.com>
2025-07-28 16:12:02 +0200

    Fix TypeScript lottie-player declaration for Vercel build

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "lottie-player": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
+export {}; 
